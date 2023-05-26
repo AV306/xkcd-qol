f25e64863183a26a16b47e8dbcc9238edce9bab3
AV3_08 <66512356+AV306@users.noreply.github.com>
2023-05-26 14:56:02 +0800

    add code

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,11 @@
-// TODO: Insert button into xkcd page linking to corresponding explainxkcd page
+const comicIndex = document.getElementById( "middleContainer" )
+            .getElementsByTagName( "a" )[10]
+            .innerText.toString()
+            .replace( "https://xkcd.com/", "" )
+            .replace( "/", "" );
+
+const explainButton = document.createElement( "li" );
+explainButton.innerHTML = `<a href="https://explainxkcd.com/${comicIndex}" target="_blank">Explain!</a>`;
+
+const topNavBar = document.getElementsByClassName( "comicNav" )[0];
+topNavBar.insertBefore( explainButton, topNavBar.childNodes.item( 7 ) );
