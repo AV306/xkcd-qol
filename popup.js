3384716d488c94c6ba38324bfc5fbce0eeb3bbf7
Blender <66512356+AV306@users.noreply.github.com>
2023-09-18 13:02:43 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 const favouritesList = document.getElementById( "favourites_list" );
 
+var favouritesData = "";
+
 window.onload = loadFavourites();
 
 function loadFavourites()
@@ -7,6 +9,12 @@ function loadFavourites()
     // Read favourites from local storaeg
     //window.localStorage.getItem( "xkcd_favourites" ).split( ";" )
     // TODO: data read logic
-    chrome.storage.sync.get( "xkcd_favourites" ).then( (value) => { console.log( value instanceof Array ); }, null );
+    chrome.storage.sync.get( ["xkcd_favourites"] ).then( readFavourites );
+    
     favouritesList.innerHTML += `<li>test</li>`;
+}
+
+function readFavourites( str )
+{
+
 }
